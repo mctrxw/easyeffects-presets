ea2ca93f492bdccfac21998fc557a7a8f7eab1a8
toneus <toneus@blackmamba.org>
2025-09-06 20:43:30 +0900

    flake: revert, my bad

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    let
+      inherit (inputs.nixpkgs)
+        lib
+        ;
+
+      jsonFiles = builtins.filter (name: lib.hasSuffix ".json" name) (
+        builtins.attrNames (builtins.readDir ./.)
+      );
+
+      createJsonAttr = fileName: {
+        name = lib.removeSuffix ".json" fileName;
+        value = builtins.fromJSON (builtins.readFile (./. + "/${fileName}"));
+      };
+    in
+    builtins.listToAttrs (map createJsonAttr jsonFiles);
+}
